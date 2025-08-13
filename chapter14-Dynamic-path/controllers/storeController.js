873ea30f0d0cab07d5b1eef6c4766cfb343a0dc8
Rajan <rk2762992@gmail.com>
2025-08-13 18:01:08 +0530

    14.2 & 14.3 Adding Home Details page

---
+++ chapter14-Dynamic-path/controllers/storeController.js
@@ -0,0 +1,57 @@
+const Home = require("../models/home");
+
+exports.getIndex = (req, res, next) => {
+  Home.fetchAll((registeredHomes) =>
+    res.render("store/index", {
+      registeredHomes: registeredHomes,
+      pageTitle: "airbnb HOme",
+      currentPage: "index",
+    })
+  );
+};
+
+exports.getHomes = (req, res, next) => {
+  Home.fetchAll((registeredHomes) =>
+    res.render("store/home-list", {
+      registeredHomes: registeredHomes,
+      pageTitle: "Home List",
+      currentPage: "home",
+    })
+  );
+};
+exports.getBookings = (req, res, next) => {
+  
+  res.render("store/bookings", {
+    pageTitle: "My Bookings",
+    currentPage: "bookings",
+  });
+};
+
+
+exports.getFavouriteList = (req, res, next) => {
+  Home.fetchAll((registeredHomes) =>
+    res.render("store/favourite-list", {
+      registeredHomes: registeredHomes,
+      pageTitle: "My Favourites",
+      currentPage: "favorites",
+    })
+  );
+};
+
+exports.getHomeDetails = (req, res, next) => {
+ const homeId = req.params.homeId;
+ Home.findById(homeId, home => {
+  if (!home) {
+    console.log("Home not found");
+    res.redirect("/homes")
+  } else {
+res.render("store/home-detail", {
+  home: home,
+      pageTitle: "Home Details",
+      currentPage: "home",
+    })
+  }
+   
+ })
+
+};
---
+++ chapter14-Dynamic-path/models/home.js
@@ -0,0 +1,40 @@
+
+const fs = require('fs');
+const path = require('path');
+const rootDir = require('../utils/pathUtil');
+const  homeDataPath  = path.join(rootDir,'data', 'homes.json');
+
+
+
+module.exports = class Home {
+ constructor(houseName, price, location, photoUrl) {
+  this.houseName = houseName;
+  this.price = price;   
+  this.location = location;
+  this.photoUrl = photoUrl;
+
+ }
+
+ save() {
+  this.id = Math.random().toString();
+ Home.fetchAll(registeredHomes => {
+  registeredHomes.push(this);
+ fs.writeFile(homeDataPath, JSON.stringify(registeredHomes), error => {
+ 
+ })
+  })
+
+ }
+ static fetchAll(callback) {
+  fs.readFile(homeDataPath, (err, data) => {
+    callback(!err ? JSON.parse(data) : []);
+  });
+ }
+
+ static findById(homeId,callback) {
+this.fetchAll(homes => {
+ const homeFound = homes.find(home => home.id === homeId);
+ callback(homeFound);
+})
+ }
+}
