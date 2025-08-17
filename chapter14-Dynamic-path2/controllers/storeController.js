d5dcff96a161a4fd590994d5692d9447119be9fb
Rajan <rk2762992@gmail.com>
2025-08-17 08:30:52 +0530

    14.4 Adding Favourites Feature

---
+++ chapter14-Dynamic-path2/controllers/storeController.js
@@ -0,0 +1,63 @@
+const Home = require("../models/home");
+
+exports.getIndex = (req, res, next) => {
+  Home.fetchAll((registeredHomes) =>
+    res.render("store/index", {
+      registeredHomes: registeredHomes,
+      pageTitle: "airbnb HOme",
+      currentPage: "index",
+    })
+  );
+};
+
+exports.getHomes = (req, res, next) => {
+  Home.fetchAll((registeredHomes) =>
+    res.render("store/home-list", {
+      registeredHomes: registeredHomes,
+      pageTitle: "Home List",
+      currentPage: "home",
+    })
+  );
+};
+exports.getBookings = (req, res, next) => {
+  
+  res.render("store/bookings", {
+    pageTitle: "My Bookings",
+    currentPage: "bookings",
+  });
+};
+
+
+exports.getFavouriteList = (req, res, next) => {
+  Home.fetchAll((registeredHomes) =>
+    res.render("store/favourite-list", {
+      registeredHomes: registeredHomes,
+      pageTitle: "My Favourites",
+      currentPage: "favorites",
+    })
+  );
+};
+
+
+exports.postAddToFavourite = (req,res,next) => {
+console.log("Came to add to Favourite", req.body);
+res.redirect("/favourites");
+}
+
+exports.getHomeDetails = (req, res, next) => {
+ const homeId = req.params.homeId;
+ Home.findById(homeId, home => {
+  if (!home) {
+    console.log("Home not found");
+    res.redirect("/homes")
+  } else {
+res.render("store/home-detail", {
+  home: home,
+      pageTitle: "Home Details",
+      currentPage: "home",
+    })
+  }
+   
+ })
+
+};
