176dfef94d738aa42d4627d6efdbae5ac5466a33
Rajan <rk2762992@gmail.com>
2025-08-22 17:19:57 +0530

    14.9 Adding Delete Feature

---
+++ chapter14-Dynamic-path7/app.js
@@ -0,0 +1,34 @@
+
+//core Module 
+const path = require('path');
+
+// External Module
+
+const  express = require('express')
+
+// Local Module
+const storeRouter = require('./routes/storeRouter');
+
+const hostRouter = require('./routes/hostRouter');
+//Local Module
+const rootdir = require("./utils/pathUtil")
+const errorsController = require("./controllers/error")
+
+const app = express();
+
+app.set('view engine', 'ejs')
+app.set('views','views')
+  
+app.use(express.urlencoded());
+
+ app.use(express.static(path.join(rootdir,"public")))
+app.use(storeRouter);
+app.use("/host",hostRouter);
+
+
+
+app.use(errorsController.pageNotFound)
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on address http://localhost:${PORT}`)
+})
